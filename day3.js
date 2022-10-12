e3e591a7e2d6f5614235e8e9fe277a8e5fae13b8
zettacamp-fahmi-fahreza <fahmi.fahreza@zettacamp.pro>
2022-10-12 16:45:18 +0700

    first commit

--- day3.js
+++ day3.js
@@ -19,6 +19,7 @@ const book = {
     finalPrice : function(){
         if (this.onSale){
             return this.priceAfterDiscount() + (this.priceAfterDiscount() * (this.tax/100));
+            // book.totaltotprice = this.finalPrice();
         }{
             return this.priceAfterTax();
         }
@@ -26,6 +27,7 @@ const book = {
 }
 let creditFunction = (book, creditMonth) =>{
     const {price} = book
+    
     let credit = Math.ceil(price /creditMonth)
     const monthlyPay = {}
     debt = credit * creditMonth
@@ -35,12 +37,19 @@ let creditFunction = (book, creditMonth) =>{
     monthlyPay.debt = debt
     let newArr = []
 
+    
+
+
+    
     for(let i = 0; i < creditMonth; i++) {
             monthlyPay.month = i +1;
             monthlyPay.debt -= credit
             monthlyPay.totalPriceCredit += credit;
             newArr.push({...monthlyPay})
     }
+    newArr.map((e)=>{
+        e.scheme=`Scheme Credit ${ e.month}`;
+    })
     console.log(newArr)
     book.monthlyPay = newArr
 }
@@ -55,39 +64,59 @@ let bookDetails = {
 
 let purchasedBook = 0
 let amountPrice = 0;
-// let totalPrice = function(wantToBuy) {
-//     for(let i= 1; i <= wantToBuy; i++){
-//         if(book.stock){
-//             amountPrice += book.finalPrice();
-//             book.stock -= 1;
-//             purchasedBook = i
+let totalPrice = function(wantToBuy) {
+    for(let i= 1; i <= wantToBuy; i++){
+        if(book.stock){
+            amountPrice += book.finalPrice();
+            book.stock -= 1;
+            purchasedBook = i
 
-//         }else{
-//             break;
-//         }
-//     }
-//     console.log(`The total price is:
-//     ${purchasedBook} x RP ${book.finalPrice()} = RP ${amountPrice}`)
-//     console.log(`Our available stock: ${book.stock}`)
-//     if(book.stock > 0){
-//         console.log(`You can still buy ${book.stock} of our book`)
-//     }else{
-//         console.log(`Im Sorry, but our book is out of stock`)
-//     }
-// }
+        }else{
+            break;
+        }
+    }
+    console.log(`The total price is:
+    ${purchasedBook} x RP ${book.finalPrice()} = RP ${amountPrice}`)
+    console.log(`Our available stock: ${book.stock}`)
+    if(book.stock > 0){
+        console.log(`You can still buy ${book.stock} of our book`)
+    }else{
+        console.log(`Im Sorry, but our book is out of stock`)
+    }
+}
 
 
-// console.log(bookDetails)
-// console.log(`The amount of discount is ${book.discount}%,
-// price after discount is RP ${book.priceAfterDiscount()},
-// The amount of tax is ${book.tax}%,
-// price after tax (without discount) is RP ${book.priceAfterTax()},
-// And the final price including discount and tax is RP ${book.finalPrice()}
-// `);
-// totalPrice(5)
-// console.log("=================================================")
+console.log(bookDetails)
+console.log(`The amount of discount is ${book.discount}%,
+price after discount is RP ${book.priceAfterDiscount()},
+The amount of tax is ${book.tax}%,
+price after tax (without discount) is RP ${book.priceAfterTax()},
+And the final price including discount and tax is RP ${book.finalPrice()}
+`);
+totalPrice(5)
+console.log("===========================================")
 creditFunction(book, 2);
+
+
+
+
+
+
+
+
+
 // console.log(book);
+// let person = {
+//     name: 'John',
+//     age:29
+// }
+// // let person2 = person
+// let person2 = person
 
+// person.age=31
+// console.log(person2);
 
-
+// testArr = [1,2,3]
+// testArr.push(5)
+// testArr.unshift(9)
+// console.log(testArr)
